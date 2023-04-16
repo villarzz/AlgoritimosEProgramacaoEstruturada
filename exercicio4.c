031fab20424a7a61006370cac6793cf2f3655750
Hugo Vilar <hugovilar04@gmail.com>
2023-04-16 11:16:24 -0300

    Finalizando exercicio 4

---
+++ exercicio4.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    float x, y;
+
+    printf("Digite um valor para X \n");
+    scanf("%f", &x);
+    
+    printf("Digite um valor para Y \n");
+    scanf("%f", &y);
+
+    if(x > 0 && y > 0){
+        printf("O ponto esta no primeiro quadrante");
+    }else if( x < 0 && y > 0 ){
+        printf("O ponto esta no segundo quadrante");
+    }else if( x < 0 && y < 0 ){
+        printf("O ponto esta no terceiro quadrante");
+    }else if(x > 0 && y < 0){
+        printf("O ponto esta no quarto quadrante");
+    }else if(x == 0){
+        printf("O ponto esta sobre o eixo Y");
+    }else if(y == 0){
+        printf("O ponto esta sobre o eixo X");
+    }else{
+        printf("O ponto esta na origem");
+    }
+
+    return 0;
+}
