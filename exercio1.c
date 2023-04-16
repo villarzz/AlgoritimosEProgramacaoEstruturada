22d0cf8ae743ddb7c72327ddd9420c6d47de3b7f
Hugo Vilar <hugovilar04@gmail.com>
2023-04-16 10:39:24 -0300

    Realizando exercicio 1 da lista

---
+++ exercio1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int A,B,C,D, resultado;
+    printf("Digite o valor de A \n");
+    scanf("%d", &A);
+    printf("Digite o valor de B \n");
+    scanf("%d", &B);
+    printf("Digite o valor de C \n");
+    scanf("%d", &C);
+    printf("Digite o valor de D \n");
+    scanf("%d", &D);
+
+    printf("Valor de %d * valor de %d - valor de %d * valor de %d \n",A,B,C,D);
+    
+    resultado = (A * B - C * D);
+    printf("O valor da diferenca eh: %d", resultado);
+
+}
