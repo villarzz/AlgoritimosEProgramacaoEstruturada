eddfc3cbc051e368849e8110033466f37efae443
Hugo Vilar <hugovilar04@gmail.com>
2023-04-16 10:53:32 -0300

    Exercicio 2

---
+++ exercicio2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    float x1,y1,x2,y2, resultado;
+    printf("Digite o primeiro valor do eixo X \n");
+    scanf("%f", &x1);
+    printf("Digite o segundo valor do eixo Y \n");
+    scanf("%f", &y1);
+    printf("Digite o segundo valor do eixo X \n");
+    scanf("%f", &x2);
+    printf("Digite o segundo valor do eixo Y \n");
+    scanf("%f", &y2);
+
+    resultado = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+
+    printf("A distancia é de %.4f", resultado);
+    
+    return 0;
+}
